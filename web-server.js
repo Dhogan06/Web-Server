00de02938f0d0a28e5a4086ef50b8c84830b5270
Dhogan06 <68254682+Dhogan06@users.noreply.github.com>
2021-01-17 13:02:33 -0700

    Add files via upload

--- web-server.js
+++ web-server.js
@@ -2,14 +2,27 @@ const http = require('http');
 const port = 3000;
 const url = require('url');
 const fs = require('fs');
+
+const colors = require('colors');
+
 const server = http.createServer(function(req, res) {
     const urladdress = url.parse(req.url, true);
     const filename = "." + urladdress.pathname;
     fs.readFile(filename, function(err, data) {
         if (err) {
-            res.write("404 File Not Found");
-            return res.end();
+           fs.readFile('404error.html', function(err1, data1) {
+                if (err1) {
+                    console.log('Error');
+                    res.write('Error: File Not Found (404error.html)');
+                    return res.end();
+                    } else {
+                        res.setHeader('Content-Type', 'text/html');
+                        res.write(data1);
+                        return res.end();
+                }
+            });
         } else {
+            console.log('Request Was Made: ' + req.url);
             res.setHeader('Content-Type', 'text/html');
             res.write(data);
             return res.end();
@@ -20,6 +33,8 @@ const server = http.createServer(function(req, res) {
 
 server.listen(port, function() {
 
-    console.log('Server Is Online Port:3000');
+    console.log('Server Is Online Port:' + port + ''.red);
+    console.log('Press Ctrl + C To Stop The Server'.green);
+    
     
 });
